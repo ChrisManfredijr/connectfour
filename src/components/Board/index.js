2ccb856855691825f8d4769feb1bcb689a6caad9
Chris <chrismanfredijr@gmail.com>
2022-10-28 20:01:42 -0400

    intial game logic

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import {ReactComponent as BoardTop} from '../../assets/images/board-layer-white-large.svg';
+import {ReactComponent as BoardBottom} from '../../assets/images/board-layer-black-large.svg';
+import './index.css';
 const Board = () => {
   return (
-    <div>index</div>
+    <div className='Board'>
+        <div className='playArea'>
+            <BoardTop className='boardTop'/>
+            <BoardBottom className='boardBottom'/>
+        </div>
+    </div>
   )
 }
 
--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import Board from '../../components/Board';
+import {useState, useReducer} from 'react';
 
 const Game = () => {
+  function reducer(state, action){
+    switch (action.type) {
+      case 'playerOne':
+        return;
+      case 'playerTwo':
+        return;
+      case 'start':
+        console.log('start');
+        return;
+      case 'reset':
+        return; 
+    }
+  }
+
+  function start(){
+    dispatch({type: 'start'});
+  }
+  
+  const [state, dispatch] = useReducer(reducer, 
+    {
+      board: [0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 
+              0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0,],
+      playerOne: 1,
+      playerTwo: 2,
+      playerOneScore: 0,
+      playerTwoScore: 0,
+    }
+    
+    )
+  
   return (
-    <div>index</div>
+    <div className='Game'>
+        <Board/>
+        <button onClick={start}>start</button>
+    </div>
   )
 }
 
