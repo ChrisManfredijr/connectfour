a2849d5a0c5dde144cc152b256e507cc7b4a417c
Chris <chrismanfredijr@gmail.com>
2022-10-29 00:40:51 -0400

    drop piece function

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import Board from '../../components/Board';
+import React, { useEffect } from 'react';
 import {useState, useReducer} from 'react';
 
 const Game = () => {
+  useEffect(() => {
+      start();
+    }, [])
+    
+  const [column, setColumn] = useState(0);
+  const [playerTurn, setPlayerTurn] = useState('P1')
+
   function reducer(state, action){
     switch (action.type) {
       case 'playerOne':
@@ -10,10 +16,10 @@ const Game = () => {
       case 'playerTwo':
         return;
       case 'start':
-        console.log('start');
         return;
       case 'reset':
         return; 
+      
     }
   }
 
@@ -35,12 +41,55 @@ const Game = () => {
       playerTwoScore: 0,
     }
     
-    )
+    );
+  
+  //test board
+  var board = [0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 
+                 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 
+                 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0,];
+
+  //game logic
+  
+
+  function dropPiece(){
+      for(let i = 5; i >= 0; i--){
+          if(board[(i*7) + column] === 0){
+              if(playerTurn === 'P1'){
+                  board[(i*7) + column] = 1;
+                  setPlayerTurn('P2');
+                  console.log(board);
+              }else if(playerTurn === 'P2'){
+                  board[(i*7) + column] = 2;
+                  setPlayerTurn('P1');
+                  console.log(board);
+              }
+              checkWin();
+              return;
+          }
+      }
+  }
+
+  function checkWin() {
+      
+  }
+  function increment() {
+      if(column < 6){
+        setColumn(prevColumn => prevColumn + 1);
+      }
+  };
+
+  function decrement() {
+      if(column > 0) {
+        setColumn(prevColumn => prevColumn - 1);
+      }
+  };
   
   return (
     <div className='Game'>
-        <Board/>
-        <button onClick={start}>start</button>
+          <button onClick={decrement}>left</button><h1>{column}</h1><button onClick={increment}>right</button><button onClick={dropPiece}>submit</button>
     </div>
   )
 }
